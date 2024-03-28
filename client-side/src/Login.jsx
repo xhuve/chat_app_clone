16a7ee7efa38be379056a34c03e935bd737f0117
xhuve <arditxhuveli@gmail.com>
2024-03-28 12:41:01 +0100

    Created login page

--- client-side/src/Login.jsx
+++ client-side/src/Login.jsx
@@ -4,7 +4,6 @@ import img from './assets/icons/icons8-messages-100.png'
 function Login() {
     
     return (
-
         <div className='flex bg-white md:h-[70%] h-screen md:w-[30%] w-[90vw] gap-7 justify-center items-center flex-col border-1 rounded-md'>
                 <div className='flex items-center gap-3 flex-col text-center font-semibold text-4xl'>
                     <img className='size-20' src={img} alt="icon" />
