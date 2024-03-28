bf779d4c6deb652e006d94e020459546d43e3905
xhuve <arditxhuveli@gmail.com>
2024-03-28 11:20:33 +0100

    Added vite for client side

---
+++ client-side/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Login from './Login'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <Login />      
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client-side/src/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login() {
+    
+    return (
+        <div className = 'flex bg-red-200 w-1/3 h-1/4 justify-center items-center flex-col'>
+                <div className = 'mb-[30%]'>
+                    Login
+                </div>
+                <div>
+                    Test
+                </div>
+        </div>
+    );
+}
+
+export default Login;
