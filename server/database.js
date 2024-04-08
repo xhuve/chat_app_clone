8e88aa28d0f54ed5cda2dc73255a02a5db753ee4
xhuve <xhuvelialbana@gmail.com>
2024-04-08 16:31:37 +0200

    Fix database

--- server/database.js
+++ server/database.js
@@ -7,31 +7,14 @@ const pool = mysql.createPool({
     database: "ChatApp"
 }).promise();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 45ec09b (Add very basic functionalities for register and login along with endpoints)
 export const getUser = async (name) => {
     const [ res ] = await pool.query(`SELECT * FROM Users WHERE username = "${name}"`)
     if (!res)
         return ("error")
     return (res);
 }
-<<<<<<< HEAD
 
 export const createUser = async (name, password) => {
     const [ res ] = await pool.query(`INSERT INTO Users (username, password) VALUES ("${name}", "${password}")`)
     return (res);
 }
-=======
-const res = await pool.query("SELECT * FROM Users")
-
-console.log(res)
->>>>>>> ce8b5f6 (Adding db)
-=======
-
-export const createUser = async (name, password) => {
-    const [ res ] = await pool.query(`INSERT INTO Users (username, password) VALUES ("${name}", "${password}")`)
-    return (res);
-}
->>>>>>> 45ec09b (Add very basic functionalities for register and login along with endpoints)
