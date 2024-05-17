203c7b7381ebcd8051e49eee82c78d51e3715f85
xhuve <xhuvelialbana@gmail.com>
2024-05-17 21:37:16 +0200

    Improve registration page layout and responsiveness

--- client-side/src/components/Register.jsx
+++ client-side/src/components/Register.jsx
@@ -22,16 +22,21 @@ function Register() {
     }
     
     return (
-        <div className='flex bg-white lg:h-[70%] lg:w-[30%] md:h-[70%] md:w-[50%] h-screen w-[90vw] gap-7 justify-center items-center flex-col border-1 rounded-md'>
-                <div className='flex items-center gap-3 flex-col text-center font-semibold text-4xl'>
-                    <img className='size-20' src={img} alt="icon" />
-                    <p>Welcome</p>
-                </div>
-                <form onSubmit={handleRegister} className='flex flex-col w-2/3 gap-3'>
-                    <input onChange={handleInput} name="username" type="text" className='border-b-2 border-gray-400 pb-2' placeholder='Username' />
-                    <input onChange={handleInput} name="password" type="text" className='border-b-2 border-gray-400 pb-2' placeholder='Password' />
-                    <button type="submit" className='bg-blue-500 text-white p-2 rounded-3xl mt-[3%]'>Register</button>
-                </form>
+        <div className='w-screen h-screen overflow-hidden flex items-center justify-between bg-gray-200'>
+            <div className='flex bg-white lg:h-[70%] lg:w-[30%] md:h-[70%] md:w-[50%] h-screen w-[90vw] md:ml-[10%] md:border-2 border-gray-300 border-solid gap-7 justify-center items-center flex-col border-1 rounded-md'>
+                    <div className='flex items-center gap-3 flex-col text-center font-semibold text-4xl'>
+                        <img className='size-20' src={img} alt="icon" />
+                        <p>Welcome</p>
+                    </div>
+                    <form onSubmit={handleRegister} className='flex flex-col w-2/3 gap-3'>
+                        <input onChange={handleInput} type="text" className='border-b-2 border-gray-400 pb-2' name='username' placeholder='Username' />
+                        <input onChange={handleInput} type="password" className='border-b-2 border-gray-400 pb-2' name='password' placeholder='Password' />
+                        <button type='submit' className='bg-blue-500 text-white p-2 rounded-3xl mt-[3%]'>Register</button>
+                    </form>
+            </div>
+            <div className='w-2/3 md:w-1/3 h-screen bg-gradient-to-l from-blue-500 to-white md:to-transparent'>
+                Test
+            </div>
         </div>
     );
 }
